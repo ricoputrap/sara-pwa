3cc81a575f079481b8e25f242d206ab3858448e3
Rico Putra Pradana <pradanaricoputra@gmail.com>
2025-01-04 22:42:00 +0700

    feat: show checkout button

---
+++ src/components/_fnb/checkout.tsx
@@ -0,0 +1,22 @@
+import useFoodBeverageStore from '@/stores/fnb-store'
+import { Button } from '../ui/button';
+
+export default function Checkout() {
+  const { orderItems } = useFoodBeverageStore();
+
+  const totalItems = orderItems.reduce((acc, item) => acc + item.quantity, 0);
+  const totalItemsLabel = totalItems > 1
+    ? `${totalItems} items`
+    : "1 item"
+
+  const totalPrice = orderItems.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+
+  return (
+    <div className="w-full border-t px-4 py-3 bg-white fixed bottom-0">
+      <Button className="w-full flex justify-between">
+        <p className="font-medium">{totalItemsLabel}</p>
+        <p>${totalPrice.toFixed(2)}</p>
+      </Button>
+    </div>
+  )
+}
--- src/components/_fnb/index.tsx
+++ src/components/_fnb/index.tsx
@@ -1,16 +1,27 @@
+import useFoodBeverageStore from '@/stores/fnb-store';
+import Checkout from './checkout';
 import { ITEMS } from './data'
 import Items from './items';
 import { EnumCategory } from './types'
+import { cn } from '@/lib/utils';
 
 const foods = ITEMS.filter((item) => item.category == EnumCategory.FOOD);
 const drinks = ITEMS.filter((item) => item.category == EnumCategory.DRINK);
 
 export default function FoodAndBeverage() {
-  
+  const { orderItems } = useFoodBeverageStore();
+
   return (
-    <div id="food-and-beverage" className="flex flex-col gap-y-2">
-      <Items title="Foods" items={foods} />
-      <Items title="Drinks" items={drinks} />
+    <div id="food-and-beverage">
+      <div className={cn({
+        "flex flex-col gap-y-2": true,
+        "pb-16": orderItems.length > 0
+      })}>
+        <Items title="Foods" items={foods} />
+        <Items title="Drinks" items={drinks} />
+      </div>
+
+      {orderItems.length > 0 ? <Checkout /> : null}
     </div>
   )
 }
